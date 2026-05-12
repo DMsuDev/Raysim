e8d13191f9b955d9807d94adbc687699c31a1531
Dayron Mustelier <dayrondotres@gmail.com>
2026-05-12 02:02:59 +0200

    chore: Add license header to multiple core and graphics header files for compliance

--- include/Raysim/Core/Application.hpp
+++ include/Raysim/Core/Application.hpp
@@ -1,3 +1,24 @@
+/**********************************************************************************************
+ *   Raysim - A C++ framework for 2D graphics and interactive applications
+ *
+ *   LICENSE: Apache License, Version 2.0
+ *
+ *            Copyright 2026 Dayron Mustelier (@DMsuDev)
+ *
+ *   Raysim is licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ **********************************************************************************************/
+
 #pragma once
 
 // Exclude conflicting Win32 definitions that clash with raylib
@@ -145,18 +166,6 @@ public:
     void Close();
 
 protected:
-
-    // -- Resources -----------------------------------------------------------
-
-    /// Load and register the default font for text rendering.
-    /// @param fontPath  Path to a TTF/OTF font file.
-    /// @param fontSize  Desired font size in pixels (default: 32).
-    void SetDefaultFont(const std::string& fontPath, int fontSize = 32);
-
-    // -- Misc ----------------------------------------------------------------
-
-    /// Seed the global RNG for reproducible results.
-    void SetRandomSeed(unsigned int seed);
 
 private:
 
