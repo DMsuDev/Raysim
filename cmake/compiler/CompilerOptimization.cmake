bc16a6c46524466b11d1ad7c1f924a8aeac2b2c3
Dayron Mustelier <dayrondotres@gmail.com>
2026-08-13 01:20:52 +0200

    refactor(cmake): clean up compiler optimization flags and improve readability

--- cmake/compiler/CompilerOptimization.cmake
+++ cmake/compiler/CompilerOptimization.cmake
@@ -16,61 +16,38 @@ function(rs_enable_optimizations target)
       "[rs] rs_enable_optimizations: target '${target}' does not exist.")
   endif()
 
-  # Shorthand generator expressions used below
-  set(_is_msvc   "$<CXX_COMPILER_ID:MSVC>")
-  set(_is_gnu    "$<CXX_COMPILER_ID:GNU>")
-  set(_is_clang  "$<CXX_COMPILER_ID:Clang,AppleClang>")
+  set(_is_msvc      "$<CXX_COMPILER_ID:MSVC>")
   set(_is_gcc_clang "$<CXX_COMPILER_ID:GNU,Clang,AppleClang>")
 
-  set(_dbg   "$<CONFIG:Debug>")
-  set(_rel   "$<CONFIG:Release>")
-  set(_rwdi  "$<CONFIG:RelWithDebInfo>")
+  set(_dbg  "$<CONFIG:Debug>")
+  set(_rel  "$<CONFIG:Release>")
+  set(_rwdi "$<CONFIG:RelWithDebInfo>")
 
   # -----------------------------------------------------------------------
   # Compile options
   # -----------------------------------------------------------------------
 
   target_compile_options("${target}" PRIVATE
-    # --- MSVC ---
-    $<$<AND:${_dbg},${_is_msvc}>:   /Od /Zi /Ob0 /RTC1>
-    $<$<AND:${_rel},${_is_msvc}>:   /O2 /Ob2 /GL>
-    $<$<AND:${_rwdi},${_is_msvc}>:  /O2 /Zi  /Ob1>
+    # MSVC: /MP (parallel compilation) not set by CMake
+    $<${_is_msvc}:/MP>
 
-    # --- GCC / Clang ---
-    $<$<AND:${_dbg},${_is_gcc_clang}>:  -O0 -g3 -fno-omit-frame-pointer>
-    $<$<AND:${_rel},${_is_gcc_clang}>:  -O3 -ffunction-sections -fdata-sections>
-    $<$<AND:${_rwdi},${_is_gcc_clang}>: -O2 -g>
+    # GCC/Clang: frame pointer in Debug for profiler/sanitizer compatibility
+    $<$<AND:${_dbg},${_is_gcc_clang}>:-fno-omit-frame-pointer>
+
+    # GCC/Clang: section splitting in Release/RelWithDebInfo enables dead-strip
+    $<$<AND:$<OR:${_rel},${_rwdi}>,${_is_gcc_clang}>:-ffunction-sections -fdata-sections>
   )
 
   # -----------------------------------------------------------------------
-  # Linker flags  (dead-strip in Release)
+  # Linker flags  (dead-strip in Release and RelWithDebInfo)
   # -----------------------------------------------------------------------
 
   target_link_options("${target}" PRIVATE
-    # MSVC
-    $<$<AND:${_rel},${_is_msvc}>: /LTCG>
+    # Linux: GCC / Clang
+    $<$<AND:$<OR:${_rel},${_rwdi}>,$<PLATFORM_ID:Linux>,$<CXX_COMPILER_ID:GNU,Clang>>:-Wl,--gc-sections>
 
-    # Clang on Windows (lld-link frontend via clang++)
-    $<$<AND:${_rel},$<PLATFORM_ID:Windows>,$<CXX_COMPILER_ID:Clang>>: -flto>
-
-    # GCC on Linux
-    $<$<AND:${_rel},$<CXX_COMPILER_ID:GNU>>: -Wl,--gc-sections>
-
-    # Clang on Linux
-    $<$<AND:${_rel},$<PLATFORM_ID:Linux>,$<CXX_COMPILER_ID:Clang>>: -Wl,--gc-sections>
-
-    # AppleClang / Clang on macOS
-    $<$<AND:${_rel},$<PLATFORM_ID:Darwin>,$<CXX_COMPILER_ID:Clang,AppleClang>>: -Wl,-dead_strip>
-  )
-
-  # -----------------------------------------------------------------------
-  # Preprocessor definitions
-  # -----------------------------------------------------------------------
-
-  target_compile_definitions("${target}" PRIVATE
-    $<${_dbg}:  _DEBUG>
-    $<${_rel}:  NDEBUG>
-    $<${_rwdi}: NDEBUG>
+    # macOS: Clang / AppleClang
+    $<$<AND:$<OR:${_rel},${_rwdi}>,$<PLATFORM_ID:Darwin>,$<CXX_COMPILER_ID:Clang,AppleClang>>:-Wl,-dead_strip>
   )
 
 endfunction()
